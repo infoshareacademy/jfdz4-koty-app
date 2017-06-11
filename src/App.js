42e7e456e57154deec8cdc9412f59ddf971de041
wojtekkas <kas@post.pl>
2017-06-11 11:38:23 +0200

    Cleaning project with proper APP.JS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import {
     Link
 } from 'react-router-dom'
 
+class App extends React.Component {
+    render() {
+        return (
+            <div>
 
+            </div>
+        )
+    }
+}
 
 
 export default App;
