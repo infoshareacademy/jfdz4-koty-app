06b5cbf1505dfd90010c5bbaf8a59952fef1c32a
wojtekkas <kas@post.pl>
2017-06-11 11:32:43 +0200

    Cleaning project

--- src/App.js
+++ src/App.js
@@ -4,41 +4,8 @@ import {
     Route,
     Link
 } from 'react-router-dom'
-import './App.css';
-import Login from './login'
-import data from './database/productsBase.json'
-import Select from 'react-select';
-import 'react-select/dist/react-select.css';
 
-const BasicExample = () => (
-    <Router>
-        <div>
-            <div style={{textAlign: 'right'}}>
-                <ul style={{listStyle: 'none'}}>
-                    <li><Link to="/LoginNoFB"><Login/></Link></li>
-                    <li><Link to="/LoginNoFB">Zaloguj bez facebooka</Link></li>
-                </ul>
-            </div>
-            <hr/>
 
-            {/*<Route exact path="/Login" component={Login}/>*/}
-            {/*<Route exact path="/LoginNoFB" component={Login}/>*/}
 
-        </div>
-    </Router>
-)
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-          <BasicExample/>
-          <div style={{width: '40%', margin: '0 auto', marginTop: '200px'}}>
-              <Select/>
-          </div>
-      </div>
-    );
-  }
-}
 
 export default App;
--- src/routing.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by MKwestorowski on 24.05.2017.
- */
