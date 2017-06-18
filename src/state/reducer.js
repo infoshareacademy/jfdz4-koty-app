4c20f29669178d3431740758fc6dbcd95695d36f
michalkwestorowski <Kwestorowski.m@gmail.com>
2017-06-18 12:34:37 +0200

    Added redux search

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,7 +1,10 @@
 import productsSearching from './parts/productsSearching'
+import products from './parts/products'
+
 
 export default (state = {}, action) => {
     return {
         productsSearching: productsSearching(state.productsSearching, action),
+        products: products(state.products, action)
     }
 }
