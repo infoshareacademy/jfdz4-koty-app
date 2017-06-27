def15250edccd2b30cdd2c9f7c876d2bf25d41af
Wojtek <kas@post.pl>
2017-06-27 12:39:13 +0200

    added some styling

--- src/components/logIn.js
+++ src/components/logIn.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import firebase from 'firebase'
 
+import './App.css'
+
 const config = {
     apiKey: "AIzaSyBEsyoqvY955QsdrdbEpGIWvNNnvZ3PC0U",
     authDomain: "koty-app-login.firebaseapp.com",
@@ -26,23 +28,23 @@ class logIn extends React.Component {
     }
 
     handleChange = event => this.setState({
-            [event.target.name]: event.target.value
+        [event.target.name]: event.target.value
     })
 
     handleSubmit = event => {
         event.preventDefault()
         firebase.auth().createUserWithEmailAndPassword(this.state.login, this.state.password).then(
-        this.setState({
-            user: firebase.auth().currentUser.email
-        }))
+            this.setState({
+                user: firebase.auth().currentUser.email
+            }))
     }
 
     handleLogin = event => {
         event.preventDefault()
         firebase.auth().signInWithEmailAndPassword(this.state.logged, this.state.passworded).then(
-        this.setState({
-            user: firebase.auth().currentUser.email
-        }))
+            this.setState({
+                user: firebase.auth().currentUser.email
+            }))
     }
 
     facebookLogin = event => {
@@ -52,8 +54,11 @@ class logIn extends React.Component {
 
 
     userLogged = () => {
+        console.log("User jako cały:")
         console.log(firebase.auth().currentUser)
+        console.log("User e-mail:")
         console.log(firebase.auth().currentUser.email)
+        console.log("User e-mail w state")
         console.log(this.state.user)
     }
 
@@ -68,7 +73,7 @@ class logIn extends React.Component {
                     user: firebase.auth().currentUser.email
                 });
             } else {
-                console.log('Not logged in')
+                console.log('Wylogowany')
             }
         });
     }
@@ -77,20 +82,40 @@ class logIn extends React.Component {
     render() {
         return (
             <div>
-                <form>
-                    <input type="text" value={this.state.login} name="login" placeholder="Login" onChange={this.handleChange} />
-                    <input type="password" value={this.state.password} name="password" placeholder="Haslo" onChange={this.handleChange} />
-                    <button type="submit" onClick={this.handleSubmit}>Zarejestruj się</button>
-                </form>
+                <div className="col-xs-12 col-sm-4  center">
+                    <div className="rejestracja">
+                    <form>
+                        <h4> ZAREJESTRUJ SIĘ </h4>
+                        <input type="text" value={this.state.login} name="login" placeholder="Login"
+                               onChange={this.handleChange}/>
+                        <input type="password" value={this.state.password} name="password" placeholder="Haslo"
+                               onChange={this.handleChange}/>
+                        <button type="submit" onClick={this.handleSubmit}>Zarejestruj się</button>
+                    </form>
+                    </div>
+                </div>
 
-                <form>
-                    <input type="text" value={this.state.logged} name="logged" placeholder="Login" onChange={this.handleChange} />
-                    <input type="password" value={this.state.passworded} name="passworded" placeholder="Haslo" onChange={this.handleChange} />
-                    <button type="submit" onClick={this.handleLogin}>Zaloguj się</button>
-                </form>
-                <button type="submit" onClick={this.facebookLogin}>Zaloguj przez facebooka</button>
-                <button onClick={this.loggingOut}>Wyloguj</button>
-                <button onClick={this.userLogged}>poka usera i email w konsoli</button>
+                <div className="col-xs-12 col-sm-4 center">
+                    <div className="logowanie">
+                    <form>
+                        <h4> ZALOGUJ SIĘ </h4>
+                        <input type="text" value={this.state.logged} name="logged" placeholder="Login"
+                               onChange={this.handleChange}/>
+                        <input type="password" value={this.state.passworded} name="passworded" placeholder="Haslo"
+                               onChange={this.handleChange}/>
+                        <button type="submit" onClick={this.handleLogin}>Zaloguj się</button>
+                    </form>
+                </div>
+                </div>
+
+                <div className="col-xs-12 col-sm-4 center">
+                    <div className="facebook">
+                    <h4> ZALOGUJ PRZEZ FACEBOOKA </h4>
+                    <button type="submit" onClick={this.facebookLogin}>Zaloguj przez facebooka</button>
+                    <button onClick={this.loggingOut}>Wyloguj</button>
+                    <button onClick={this.userLogged}>poka usera i jego email w konsoli</button>
+                    </div>
+                </div>
             </div>
         )
     }
