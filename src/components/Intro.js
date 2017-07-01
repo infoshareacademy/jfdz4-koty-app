e08624725f54f61981c107bd1860143c66234fdc
Gaszewska <gosiagaszewska@gmail.com>
2017-07-02 01:32:45 +0200

    Merge branch 'txt_pic_button4intro' of /Users/malgorzatagaszewska/WebstormProjects/jfdz4-koty-app with conflicts.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,16 +1,36 @@
 import React from 'react'
+import YouTube from 'react-youtube'
+
+const opts = {
+        width: '100%',
+    playerVars: {
+        autoplay: 0,
+        modestbranding: 1,
+        showinfo: 0,
+        rel: 0,
+    }
+}
 
 export default class Intro extends React.Component {
+
+
     render () {
         return (
             <div style={{textAlign: 'center'}}>
-                Aplikacja, która pozwala na oszczędności!
-                Wyszukujemy najlepszej jakości i w dobrej cenie produkty topowych marek.
-                Wystarczy, że wpiszesz nazwę produktu, którego szukasz i możesz oddać się szalonym zakupom wśród rasowych kotów.
-                Enjoy:)
+                <h1>KOTY APP - PORÓWNYWARKA CENOWA</h1>
+                <p>Aplikacja, która pozwala Ci oszczędzić!</p>
+                <p>Znajdź produkty najlepszej jakości topowych marek w dobrej cenie. </p>
+                <p>Wystarczy, że wpiszesz nazwę produktu, którego szukasz i możesz oddać się szalonym zakupom wśród rasowych kotów.</p>
+                <p>Enjoy:)</p>
 
-                <img style={{width: '100%', borderRadius:'20px'}} src="https://raw.githubusercontent.com/infoshareacademy/jfdz4-koty-app/txt_pic_button4intro/public/data/image/hajs.jpg" alt="kupa szmalu"/>
-
+                {/*<img style={{width: '100%', borderRadius:'20px'}} src="https://raw.githubusercontent.com/infoshareacademy/jfdz4-koty-app/txt_pic_button4intro/public/data/image/hajs.jpg" alt="kupa szmalu"/>*/}
+                <div>
+                <YouTube
+                    videoId="JnJbDhdWfqk"
+                    opts={opts}
+                    onReady={this._onReady}
+                />
+                </div>
             </div>
 
 
