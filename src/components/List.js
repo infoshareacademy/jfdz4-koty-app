9e9a2bf98d25d53fe6cc4429a62f7c2df5f25ef2
Michał <kwestorowski.m@gmail.com>
2017-07-01 11:45:26 +0200

    Need to change component state to re-render

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { Table } from 'react-bootstrap'
+import {connect} from 'react-redux'
+import {Table} from 'react-bootstrap'
 
 
 import './App.css'
@@ -19,38 +19,36 @@ export default connect(
 
             const favoriteProduct = this.props.addFavorite
 
-             return (
-                 <Table striped bordered condensed hover style={{textAlign: 'center'}}>
-                     <thead>
-                     <tr>
-                         <th>ZDJĘCIE</th>
-                         <th>NAZWA PRODUKTU</th>
-                         <th>CENA</th>
-                         <th>OCENA</th>
-                         <th>AKCJE</th>
-                     </tr>
-                     </thead>
-                     <tbody >
-                     {
+            return (
+                <Table striped bordered condensed hover style={{textAlign: 'center'}}>
+                    <thead>
+                    <tr>
+                        <th>ZDJĘCIE</th>
+                        <th>NAZWA PRODUKTU</th>
+                        <th>CENA</th>
+                        <th>OCENA</th>
+                        <th>AKCJE</th>
+                    </tr>
+                    </thead>
+                    <tbody >
+                    {
 
-                         console.log(favoriteProduct),
-                       <tr>
-                                     <td style={{verticalAlign: 'middle'}}><img src={favoriteProduct.image}/></td>
-                                     <td style={{verticalAlign: 'middle'}}>{favoriteProduct.name}</td>
-                                     <td style={{verticalAlign: 'middle'}}><p><strong>Allegro: </strong>{favoriteProduct.price_allegro}</p><p><strong>Ceneo: </strong>{favoriteProduct.price_ceneo}</p><p><strong>Ebay: </strong>{favoriteProduct.price_ebay}</p></td>
-                                     <td style={{verticalAlign: 'middle'}}>{favoriteProduct.review} / 5</td>
-                                     <td style={{verticalAlign: 'middle'}}>
-                                         <p><Link to={'/products/' + favoriteProduct.id}>Szczegóły</Link></p>
+                        console.log(favoriteProduct),
+                        <tr>
+                        <td style={{verticalAlign: 'middle'}}><img src={favoriteProduct.image}/></td>
+                        <td style={{verticalAlign: 'middle'}}>{favoriteProduct.name}</td>
+                        <td style={{verticalAlign: 'middle'}}><p><strong>Allegro: </strong>{favoriteProduct.price_allegro}</p><p><strong>Ceneo: </strong>{favoriteProduct.price_ceneo}</p><p><strong>Ebay: </strong>{favoriteProduct.price_ebay}</p></td>
+                        <td style={{verticalAlign: 'middle'}}>{favoriteProduct.review} / 5</td>
+                        <td style={{verticalAlign: 'middle'}}>
+                        <p><Link to={'/products/' + favoriteProduct.id}>Szczegóły</Link></p>
 
-                                     </td>
-                                 </tr>
+                        </td>
+                        </tr>
 
-                     }
-                     </tbody>
-                 </Table>
-             )
-
-
+                    }
+                    </tbody>
+                </Table>
+            )
 
 
         }
