6af3b2777dd26c53e221f3c6466bb7623bdf26d5
Wojtek <kas@post.pl>
2017-07-01 17:03:14 +0200

    added loader

---
+++ src/components/startSearch.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './loader.css'
+import { browserHistory } from 'react-router'
+
+export default class startSearch extends React.Component {
+    render() {
+        return (
+
+        <div className="loader">
+            {
+                setTimeout( () => (this.props.history.push('/Search')), 1500)
+            }
+        </div>
+
+        )
+    }
+}
