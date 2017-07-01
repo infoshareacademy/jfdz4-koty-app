031ef76f6ebbedb4d1dc95a78f77d670e0af41aa
Gaszewska <gosiagaszewska@gmail.com>
2017-07-01 16:14:49 +0200

    txt @intro & button redirecting to app and visual

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,8 +4,16 @@ export default class Intro extends React.Component {
     render () {
         return (
             <div>
-                aaaaa
+                Super aplikacja, która pozwala zaoszczędzić w cholerę kasy!
+                Wyszukujemy najlepszej jakości i w dobrej cenie produkty topowych polskich marek.
+                Wystarczy, że wpiszesz nazwę produktu, którego szukasz i możesz oddać się szalonym zakupom wśród rasowych kotów.
+                Enjoy:)
+
+                <button>Przejdź do aplikacji</button>
+                <img src="jfdz4-koty-app/public/data/image/hajs.jpg" alt="kupa szmalu"/>
             </div>
+
+
         )
     }
 }
