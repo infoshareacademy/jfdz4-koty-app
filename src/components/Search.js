f1a5be6e85b4126c7c4598d015a02300f5859513
Wojtek <kas@post.pl>
2017-07-01 12:15:54 +0200

    added tr hover

--- src/components/Search.js
+++ src/components/Search.js
@@ -44,7 +44,7 @@ export default connect(
                         <ul>
                             {
 
-                                            <Table striped bordered condensed hover style={{textAlign: 'center'}}>
+                                            <Table striped bordered condensed style={{textAlign: 'center'}}>
                                                 <thead>
                                                 <tr>
                                                     <th>ZDJĘCIE</th>
@@ -67,7 +67,7 @@ export default connect(
                                                             )
                                                         ).slice(0, 10).map(
                                                         product => <tr key={product.id}>
-                                                            <td style={{verticalAlign: 'middle'}}><img style={{borderRadius: '15px'}} alt="costam" src={product.image}/></td>
+                                                            <td style={{verticalAlign: 'middle'}}><img style={{borderRadius: '10px', border: "1px #21324f solid"}} alt="costam" src={product.image}/></td>
                                                             <td style={{verticalAlign: 'middle'}}>{product.name}</td>
                                                             <td style={{verticalAlign: 'middle'}}><p><strong>Allegro: </strong>{product.price_allegro}</p><p><strong>Ceneo: </strong>{product.price_ceneo}</p><p><strong>Ebay: </strong>{product.price_ebay}</p></td>
                                                             <td style={{verticalAlign: 'middle'}}>{product.review} / 5</td>
