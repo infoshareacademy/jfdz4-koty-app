9c2a683c866c501e0969a6f079309ab4e0d66043
Gaszewska <gosiagaszewska@gmail.com>
2017-07-01 18:29:28 +0200

    resposiveness, button color change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,15 +3,18 @@ import React from 'react'
 export default class Intro extends React.Component {
     render () {
         return (
-            <div>
-                Super aplikacja, która pozwala zaoszczędzić w cholerę kasy!
-                Wyszukujemy najlepszej jakości i w dobrej cenie produkty topowych polskich marek.
+            <div style={{textAlign: 'center'}}>
+                Aplikacja, która pozwala na oszczędności!
+                Wyszukujemy najlepszej jakości i w dobrej cenie produkty topowych marek.
                 Wystarczy, że wpiszesz nazwę produktu, którego szukasz i możesz oddać się szalonym zakupom wśród rasowych kotów.
                 Enjoy:)
 
-                <button>Przejdź do aplikacji</button>
-                <img src="https://raw.githubusercontent.com/infoshareacademy/jfdz4-koty-app/txt_pic_button4intro/public/data/image/hajs.jpg" alt="kupa szmalu"/>
+                <img style={{width: '100%', borderRadius:'20px'}} src="https://raw.githubusercontent.com/infoshareacademy/jfdz4-koty-app/txt_pic_button4intro/public/data/image/hajs.jpg" alt="kupa szmalu"/>
+
+                <button style={{background: '#ff7d00', text: 'grey', borderRadius:'20px' }}>PRZEJDŹ DO APLIKACJI
+                </button>
             </div>
+
 
 
         )
