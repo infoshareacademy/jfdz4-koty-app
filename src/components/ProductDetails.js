ee2203756b30b57d77aaaf86687e890202ec5159
michalkwestorowski <Kwestorowski.m@gmail.com>
2017-07-02 09:32:47 +0200

    Fixed back arrow to route link

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 class Product extends React.Component {
 
@@ -35,7 +36,7 @@ class Product extends React.Component {
             <div style={{color: 'white', background: '#000d1a', boxShadow: '9px 9px 29px #21324f'}}>
                 {product === undefined ? <div> Fetching</div> :
                     <div style={{border: '2px solid #FF7D00'}}>
-                        <a href="/Search"><img src="https://raw.githubusercontent.com/infoshareacademy/jfdz4-koty-app/productDetails-style/public/data/image/arrowBack.jpg" style={{float: 'left', height: '40px', width: '40px', marginRight: '20px'}}/></a>
+                        <Link to='/Search'><img src="https://raw.githubusercontent.com/infoshareacademy/jfdz4-koty-app/productDetails-style/public/data/image/arrowBack.jpg" style={{float: 'left', height: '40px', width: '40px', marginRight: '20px'}}/></Link>
                         <h1 style={{fontFamily: 'Verdana', fontWeight: '800', color: '#FF7D00'}}> {product.name} </h1>
                         <img src={product.image} style={{marginLeft: '4px', marginBottom: '8px', border: '1px solid #999999', boxShadow: '2px 2px 22px'}}/>
                         <p style = {{fontFamily: 'Verdana', fontWeight: '700', fontSize: '27px', color: '#FF7D00'}}>Cena: {product.price}</p>
